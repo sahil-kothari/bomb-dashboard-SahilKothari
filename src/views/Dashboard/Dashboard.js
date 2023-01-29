7c8d1c89140be1f582102e3cc7359c19f3d85f63
sahil-kothari <sahilckothari94@gmail.com>
2023-01-29 20:03:15 +0530

    changes___

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,29 +1,53 @@
 import React from 'react'
+import useTotalValueLocked from '../../hooks/useTotalValueLocked';
+import useStakedBalanceOnBoardroom from '../../hooks/useStakedBalanceOnBoardroom';
+import useBondsPurchasable from '../../hooks/useBondsPurchasable';
+import {getDisplayBalance} from '../../utils/formatBalance';
+import useBondStats from '../../hooks/useBondStats';
+import ExchangeStat from './ExchangeStat';
 
 function Dashboard() {
+
+  const TVL = useTotalValueLocked();
+  const stakedBalance = useStakedBalanceOnBoardroom();
+  const bondsPurchasable = useBondsPurchasable();
+  const bondStat = useBondStats();
+
+
   return (
     <div className="h-screen w-full">
       <div class="flex justify-center flex-col gap-4 p-8">
         <div className="grid grid-cols-3 gap-4 py-2 shadow-xl w-full bg-black bg-opacity-40 hover:bg-opacity-25">
           <div className="col-span-2 flex flex-col h-full gap-2">
-            <button
-              type="button"
-              className="w-full px-6  py-2.5 border-2 solid text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-            >
-              Invest Now
-            </button>
-            <div className="w-full flex gap-4">
+            
               <button
                 type="button"
-                className="w-1/2 px-6 py-2.5 border-2 solid text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                className="w-full px-6  py-2.5 border-2 solid text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
-                Chat on Discord
+                Invest Now
               </button>
+            
+            <div className="w-full flex gap-4">
+
+            
+              <button 
+                
+                type="button"
+                className="w-1/2 px-6 py-2.5 border-2 solid text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                <a href="https://discord.bomb.money">
+                Chat on Discord
+                </a>
+              </button>
+           
+
               <button
                 type="button"
                 className="w-1/2 px-6 py-2.5 border-2 solid text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
+                <a href="https://docs.bomb.money">
                 Read Docs
+                </a>
               </button>
             </div>
             <div className='p-6 w-full rounded-2xl border-opacity-30 border-2 bg-black bg-opacity-40 hover:bg-opacity-25'>
@@ -33,14 +57,15 @@ function Dashboard() {
                   Recommended
                 </span>
                 <div className="w-2/3"></div>
-                <span className="text-white text-lg">TVL: $1,008,430</span>
+                <span className="text-white text-lg">TVL: {TVL} </span>
               </div>
               <p class="text-gray-100 text-base mb-4">
-                Stake your LP tokens in our farms to start earning $BSHARE
+                Stake BSHARE and earn BOMB every epoch
               </p>
-              <div className="grid grid-cols-3 gap-2 text-white text-2xl mt-4">
-                <div className="">Current Price: (Bomb)^2</div>
-                <div className="">Available to redeem: </div>
+              <div className="grid grid-cols-4 gap-3 text-white text-2xl mt-3">
+                <div className="">Daily Returns</div>
+                <div className="">Your Stake </div>
+                <div className="">Earned </div>
                 <div className="flex flex-col justify-around content-center gap-4 row-span-2">
                   <div className="w-full flex justify-between items-center">
                     <span>Purchase BBond</span>
@@ -64,7 +89,7 @@ function Dashboard() {
                   </div>
                   </div>
                 </div>
-                <div className="text-xl text-slate-300">BBond = 6.2872 BTCB</div>
+                <div className="text-xl text-slate-300">2%</div>
                 <div className="text-xl text-slate-300">456</div>
               </div>
             </div>
@@ -112,13 +137,13 @@ function Dashboard() {
               type="button"
               class="px-6 mt-8 py-2.5 border-2 solid rounded-2xl text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >
-              Claim All
+              Deposit
             </button>
             <button
               type="button"
               class="px-6 ml-4 mt-8 py-2.5 border-2 solid rounded-2xl text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >
-              Claim All
+              Withdraw
             </button>
             <button
               type="button"
@@ -152,13 +177,13 @@ function Dashboard() {
               type="button"
               class="px-6 mt-8 py-2.5 border-2 solid rounded-2xl text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >
-              Claim All
+              Deposit
             </button>
             <button
               type="button"
               class="px-6 ml-4 mt-8 py-2.5 border-2 solid rounded-2xl text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >
-              Claim All
+              Withdraw
             </button>
             <button
               type="button"
@@ -173,12 +198,12 @@ function Dashboard() {
           <div className='p-6'>
             <div className=" flex">
               <span class="text-gray-100 text-xl leading-tight font-medium ">
-                Bomb Farms
+                BONDS
               </span>
               <div className="w-5/6"></div>
             </div>
             <p class="text-gray-100 text-base mb-4">
-              Stake your LP tokens in our farms to start earning $BSHARE
+              BBond can be purchased only on contraction periods, when TWAP of BOMB is below 1
             </p>
             <div className="grid grid-cols-3 gap-2 text-white text-2xl mt-4">
               <div className="">Current Price: (Bomb)^2</div>
@@ -206,8 +231,13 @@ function Dashboard() {
                 </div>
                 </div>
               </div>
-              <div className="text-xl text-slate-300">BBond = 6.2872 BTCB</div>
-              <div className="text-xl text-slate-300">456</div>
+              <div className="text-xl text-slate-300">
+                <ExchangeStat
+                  tokenName="10,000 BBOND"
+                  //description="Current Price: (BOMB)^2"
+                  price={Number(bondStat?.tokenInFtm).toFixed(4) || '-'}
+                /></div>
+              <div className="text-xl text-slate-300">{getDisplayBalance(bondsPurchasable, 18, 4)}</div>
             </div>
           </div>
         </div>
